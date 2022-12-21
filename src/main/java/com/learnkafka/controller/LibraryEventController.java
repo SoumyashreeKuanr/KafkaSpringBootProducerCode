52b8be8f36d5c4cee3de7ddc21618f5dc3a48f23
user <devsoumya95@gmail.com>
2022-12-21 12:06:44 +0530

    Adding files

---
+++ src/main/java/com/learnkafka/controller/LibraryEventController.java
@@ -0,0 +1,37 @@
+package com.learnkafka.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.learnkafka.domain.LibraryEvent;
+import com.learnkafka.domain.LibraryEventType;
+import com.learnkafka.producer.LibraryEventProducer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@Slf4j
+public class LibraryEventController {
+    @Autowired
+    LibraryEventProducer libraryEventProducer;
+    @PostMapping("/v1/libraryevent")
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody @Valid  LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException {
+     log.info("before send libraryevent");
+        //libraryEventProducer.sendLibraryEvent(libraryEvent);
+       // SendResult<Integer, String> sendResult = libraryEventProducer.sendLibraryEventSynchronous(libraryEvent);
+        //log.info("SendResult is {} ", sendResult.toString());
+        libraryEvent.setLibraryEventType(LibraryEventType.NEW);
+
+        libraryEventProducer.sendLibraryEvent_Approach2(libraryEvent);
+
+        log.info("after send libraryevent");
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+}
---
+++ src/main/java/com/learnkafka/domain/Book.java
@@ -0,0 +1,21 @@
+package com.learnkafka.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import javax.validation.constraints.NotBlank;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Book {
+    @NotNull
+    private Integer bookId;
+    @NotBlank
+    private String bookName;
+    @NotBlank
+    private String bookAuthor;
+}
